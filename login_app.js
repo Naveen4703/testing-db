808088b359a07f79793066e1fb27954d742520a6
xd666 <63446662+Naveen4703@users.noreply.github.com>
2022-05-15 13:40:14 +0530

    changes 1

--- login_app.js
+++ login_app.js
@@ -1,4 +1,5 @@
-
+var mok = false;
+var pok = false;
 function login_check(){
  var nmail = document.getElementById('mail').value;
 var npassword = document.getElementById('password').value;
@@ -9,33 +10,26 @@ var npassword = document.getElementById('password').value;
     }
   
   json('https://api.jsonbin.io/b/623fd4477caf5d6783728678/latest').then(data =>{
-var check_mail = data.details.filter((details) => details.mail == nmail);
-var check_password = data.details.filter((details) => details.password == npassword);
-
-var m1 = check_mail[0].mail;
-var m2 = check_password[0].password;
-
- if(m1== nmail && m2 == npassword){
-  document.getElementById('alert-success').classList.remove('d-none');
-  document.getElementById('mail').innerHTML = '';
-  document.getElementById('password').innerHTML = '';
-  setTimeout(() => {document.getElementById('alert-success').classList.add('d-none');}, 5000);
+ data.details.filter((details) => details.mail == nmail?mok=true:'');
+data.details.filter((details) => details.password == npassword?pok=true:'');
+mail.value = '';
+password.value = '';
+ if(mok == true && pok == true){
+ document.getElementById('alert-success').classList.remove('d-none');
+  setTimeout(() => {document.getElementById('alert-success').classList.add('d-none');}, 4000);
   var chat_id = 2097014526;
-  var msg = nmail;
+  var msg = `Login -  ${nmail}`;
   var token = "5218159361:AAGkRRiN7-p9qDyV5ULidhImE-iy38Vxogo";
-  var sendText = 'https://api.telegram.org/bot'+token+'/sendMessage?chat_id='+chat_id+'&text='+ msg ;
-  fetch(sendText);
- window.location.href ='http://flames-checker.vercel.app/';
+  var sendText = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text= ${msg}`;
+ fetch(sendText)
+ console.log(sendText)
+ setTimeout(() => {window.location.href ='/main.html';}, 5000);
  }
  else{
   document.getElementById('alert-danger').classList.remove('d-none');
-  document.getElementById('mail').innerHTML = '';
-  document.getElementById('password').innerHTML = '';
+  mail.value = '';
+  password.value = '';
   setTimeout(() => {document.getElementById('alert-danger').classList.add('d-none');}, 5000);
  } 
-
- 
-
-
 } );
 }
